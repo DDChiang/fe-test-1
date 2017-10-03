6b36554378bf5d6a9ed52aa108759f17b0a6261b
DDChiang <ddeechng@gmail.com>
2017-10-03 09:10:50 -0700

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,34 @@
+console.log('hi');
+
+var data = [
+  {
+    name: 'Joe Black',
+    company: 'Apple',
+    years: 2
+  },
+  {
+    name: 'Adam Black',
+    company: 'Apple',
+    years: 7
+  },
+  {
+    name: 'Eric Schmidt',
+    company: 'Microsoft',
+    years: 4
+  },
+  {
+    name: 'Amy Gonzalez',
+    company: 'Palantir',
+    years: 4
+  },
+  {
+    name: 'Kill Bill',
+    company: 'Book Pub',
+    years: 1
+  },
+  {
+    name: 'Kel Oppo',
+    company: 'Experian',
+    years: 6
+  }
+];
